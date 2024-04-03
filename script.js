8d0ec56677e331c55294ae29c4432372efc58f2b
ANISH KUMAR <70019437+Amish2417@users.noreply.github.com>
2024-04-03 15:21:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+if(arr.lenth==0 || arr.length==1){
+	return -Infinity;
+}
+ let reversearr = arr.reverse();
+if(reversearr[reversearr.length-2] != reverse[reverse.length-1] ){
+	return reversearr[reversearr.length-2];
+}
+else{
+	return -Infinity;
+}
